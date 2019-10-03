35ee58bb2feefa6556de163f467b0c236a77b1d6
JoaoPaulo <joaopaulobiesek@gmail.com>
2019-10-03 17:41:58 -0300

    EnumStudentRegistred

--- Models/Enums/StudentRegistration.cs
+++ Models/Enums/StudentRegistration.cs
@@ -1,9 +1,22 @@
-﻿
+﻿using InfinitySO.Models.Enums.Methods;
+
 namespace InfinitySO.Models.Enums
 {
     public enum StudentRegistration : int
     {
-        UnRegistered = 0,
-        Registered = 1
+        [EnumDisplayName(DisplayName = "Vestibular")]
+        EntranceExam = 0,
+        [EnumDisplayName(DisplayName = "Matrícula Pré-confirmada")]
+        Pre_ConfirmedRegistration = 1,
+        [EnumDisplayName(DisplayName = "Matrícula Ativa")]
+        ActiveRegistration = 2,
+        [EnumDisplayName(DisplayName = "Formando")]
+        Forming = 3,
+        [EnumDisplayName(DisplayName = "Desistente")]
+        Quitter = 4,
+        [EnumDisplayName(DisplayName = "Matrícula Trancada")]
+        LockedRegistration = 5,
+        [EnumDisplayName(DisplayName = "Matrícula Cancelada")]
+        RegistrationCanceled = 6
     }
 }
